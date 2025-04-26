d1572d2eafa8b7253ad55b70feef4b119a402612
ansari-1417 <moizansari2005@gmail.com>
2025-04-26 19:17:21 +0500

    Added buttons

--- welcome_form.Designer.cs
+++ welcome_form.Designer.cs
@@ -32,17 +32,20 @@
             this.logo_main = new System.Windows.Forms.PictureBox();
             this.tagline = new System.Windows.Forms.Label();
             this.signup_button = new System.Windows.Forms.Button();
+            this.login_button = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.logo_main)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // logo_main
             // 
             this.logo_main.BackColor = System.Drawing.Color.Transparent;
-            this.logo_main.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.logo_main.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.logo_main.Image = global::DB_Project.Properties.Resources.logo5;
-            this.logo_main.Location = new System.Drawing.Point(297, 26);
+            this.logo_main.Location = new System.Drawing.Point(0, 0);
             this.logo_main.Name = "logo_main";
-            this.logo_main.Size = new System.Drawing.Size(306, 194);
+            this.logo_main.Size = new System.Drawing.Size(693, 194);
             this.logo_main.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.logo_main.TabIndex = 0;
             this.logo_main.TabStop = false;
@@ -53,7 +56,7 @@
             this.tagline.BackColor = System.Drawing.Color.Transparent;
             this.tagline.Font = new System.Drawing.Font("Candara Light", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tagline.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(236)))), ((int)(((byte)(239)))));
-            this.tagline.Location = new System.Drawing.Point(112, 233);
+            this.tagline.Location = new System.Drawing.Point(1, 219);
             this.tagline.Name = "tagline";
             this.tagline.Size = new System.Drawing.Size(698, 54);
             this.tagline.TabIndex = 1;
@@ -61,33 +64,64 @@
             // 
             // signup_button
             // 
-            this.signup_button.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.signup_button.Location = new System.Drawing.Point(258, 320);
+            this.signup_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(230)))), ((int)(((byte)(160)))));
+            this.signup_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.signup_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signup_button.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signup_button.Location = new System.Drawing.Point(161, 296);
             this.signup_button.Name = "signup_button";
-            this.signup_button.Size = new System.Drawing.Size(128, 61);
+            this.signup_button.Size = new System.Drawing.Size(130, 40);
             this.signup_button.TabIndex = 2;
             this.signup_button.Text = "Sign Up";
-            this.signup_button.UseVisualStyleBackColor = true;
+            this.signup_button.UseVisualStyleBackColor = false;
             this.signup_button.Click += new System.EventHandler(this.signup_button_Click);
+            // 
+            // login_button
+            // 
+            this.login_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(198)))), ((int)(((byte)(255)))));
+            this.login_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.login_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.login_button.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_button.Location = new System.Drawing.Point(404, 296);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(130, 40);
+            this.login_button.TabIndex = 3;
+            this.login_button.Text = "Log In";
+            this.login_button.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.login_button);
+            this.panel1.Controls.Add(this.logo_main);
+            this.panel1.Controls.Add(this.signup_button);
+            this.panel1.Controls.Add(this.tagline);
+            this.panel1.Location = new System.Drawing.Point(98, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(699, 403);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // welcome_window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
             this.BackgroundImage = global::DB_Project.Properties.Resources.airplane3;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(908, 492);
-            this.Controls.Add(this.signup_button);
-            this.Controls.Add(this.tagline);
-            this.Controls.Add(this.logo_main);
+            this.ClientSize = new System.Drawing.Size(878, 492);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "welcome_window";
             this.Text = "Welcome";
             ((System.ComponentModel.ISupportInitialize)(this.logo_main)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -96,6 +130,8 @@
         private System.Windows.Forms.PictureBox logo_main;
         private System.Windows.Forms.Label tagline;
         private System.Windows.Forms.Button signup_button;
+        private System.Windows.Forms.Button login_button;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
---
+++ welcome_form.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB_Project
+{
+    public partial class welcome_window : Form
+    {
+        public welcome_window()
+        {
+            InitializeComponent();
+
+            // Sign Up Button
+            //signup_button.FlatStyle = FlatStyle.Flat;
+            //signup_button.BackColor = Color.FromArgb(84, 230, 160); // Mint Green
+            //signup_button.ForeColor = Color.FromArgb(232, 236, 239); // Off-White
+            //signup_button.Font = new Font("Calibri", 12, FontStyle.Bold);
+            //signup_button.Size = new Size(130, 40);
+            signup_button.FlatAppearance.BorderSize = 0;
+            signup_button.Text = "Sign Up";
+            // Add icon (if using a custom image)
+            // signup_button.Image = Image.FromFile("path_to_arrow_icon.png");
+            // signup_button.ImageAlign = ContentAlignment.MiddleLeft;
+            // signup_button.TextAlign = ContentAlignment.MiddleCenter;
+
+            // Login Button
+            //login_button.FlatStyle = FlatStyle.Flat;
+            //login_button.BackColor = Color.FromArgb(106, 176, 255); // Light Sky Blue
+            //login_button.ForeColor = Color.FromArgb(232, 236, 239); // Off-White
+            //login_button.Font = new Font("Calibri", 12, FontStyle.Bold);
+            //login_button.Size = new Size(130, 40);
+            login_button.FlatAppearance.BorderSize = 0;
+            //login_button.Text = "Login";
+            //// Add icon (if using a custom image)
+            //// login_button.Image = Image.FromFile("path_to_arrow_icon.png");
+            //// login_button.ImageAlign = ContentAlignment.MiddleLeft;
+            //// login_button.TextAlign = ContentAlignment.MiddleCenter;
+
+            // Apply rounded corners
+            SetButtonRoundedCorners(signup_button);
+            SetButtonRoundedCorners(login_button);
+        }
+
+
+    // Method for rounded corners
+    private void SetButtonRoundedCorners(Button button)
+    {
+        int radius = 20;
+        GraphicsPath path = new GraphicsPath();
+        Rectangle bounds = new Rectangle(0, 0, button.Width, button.Height);
+        int diameter = radius * 2;
+        path.AddArc(bounds.X, bounds.Y, diameter, diameter, 180, 90);
+        path.AddArc(bounds.Width - diameter, bounds.Y, diameter, diameter, 270, 90);
+        path.AddArc(bounds.Width - diameter, bounds.Height - diameter, diameter, diameter, 0, 90);
+        path.AddArc(bounds.X, bounds.Height - diameter, diameter, diameter, 90, 90);
+        path.CloseFigure();
+        button.Region = new Region(path);
+    }
+
+    private void signup_button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
